55dc304b1759fd554d64585199f9a51e68489755
fitg <gustaw.fit@gmail.com>
2019-11-05 15:27:50 +0000

    Add files via upload

---
+++ template-api_java/src/main/java/com/fitg/teleport/App.java
@@ -0,0 +1,13 @@
+package com.fitg.teleport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
