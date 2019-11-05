831a1e706f629eda7d5dee8e32e960e9a0b8715f
fitg <gustaw.fit@gmail.com>
2019-11-05 17:19:13 +0000

    Add files via upload

--- template-api_java/src/main/java/com/fitg/teleport/App.java
+++ template-api_java/src/main/java/com/fitg/teleport/App.java
@@ -1,4 +1,14 @@
 package com.fitg.teleport;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Hello world!
@@ -6,8 +16,9 @@ package com.fitg.teleport;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
+
     }
 }
---
+++ template-api_java/src/test/java/com/fitg/teleport/JsonReaderTest.java
@@ -0,0 +1,38 @@
+package com.fitg.teleport;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class JsonReaderTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public JsonReaderTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( JsonReaderTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
